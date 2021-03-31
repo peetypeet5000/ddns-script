4b24057bc9f644b5186aa2e6f63ddd1c97745c05
Peter LaMontagne <plamon42@gmail.com>
2021-03-30 17:39:44 -0700

    Initialize repo

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+### Google Domains provides an API to update a DNS
+### "Synthetic record". This script updates a record with 
+### the script-runner's public IP address, as resolved using a DNS
+### lookup.
+###
+### Google Dynamic DNS: https://support.google.com/domains/answer/6147083
+### Synthetic Records: https://support.google.com/domains/answer/6069273
+
+USERNAME="91WNgrpXYqOy5aVG"
+PASSWORD="WIrwxzfme36ctp3Q"
+HOSTNAME="raspi.peterlamontagne.com"
+DATE=$(date +"%d/%m@%H:%M")
+
+
+# Resolve current public IP
+IP=$( dig +short myip.opendns.com @resolver1.opendns.com )
+# Update Google DNS Record
+URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${HOSTNAME}&myip=${IP}"
+OUTPUT=$(curl -s $URL)
+
+printf "Time:%s  " $DATE >> log.txt
+printf "%s  " $OUTPUT >> log.txt
+printf "\n\n\n" >> log.txt
+
+
