ba971b46974c234b8753f1030cdc7e0541783b7d
Peter LaMontagne <plamon42@gmail.com>
2025-07-23 05:39:46 +0100

    update for cloudflare

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ddns-script
-A script for updating the IP address to my webserver in Google Domains. Used on my Raspberry Pi Webserver to enable access to peterlamontagne.com
+A script for updating the IP address to my webserver on Cloudflare. Used on my Raspberry Pi Webserver to enable access to peterlamontagne.com.
 
 ## Usage
-Reccomend adding to crontab to run hourly. Will output to log.txt in same directory.
+Reccomend adding to crontab to run hourly and piping the output to a log file.
+
+Example crontab entry:
+`0 * * * * ~/ddns-script/script.sh >> log.txt`
+
+Update the log file location as required and use the full file path if not using your user account to run cron.
--- script.sh
+++ script.sh
@@ -5,23 +5,52 @@
 ### the script-runner's public IP address, as resolved using a DNS
 ### lookup.
 ###
-### Google Dynamic DNS: https://support.google.com/domains/answer/6147083
-### Synthetic Records: https://support.google.com/domains/answer/6069273
+### Cloudflare API Docs: https://developers.cloudflare.com/api/resources/dns/subresources/records/methods/scan/
 
-USERNAME=""
-PASSWORD=""
-HOSTNAME="raspi.peterlamontagne.com"
-DATE=$(date +"%d/%m@%H:%M")
+ZONE_ID="your_cloudflare_zone_id"
+RECORD_ID="your_cloudflare_record_id"
+API_TOKEN="your_cloudflare_api_token"
+DNS_NAME="your_dns_name"
+TTL=120
+PROXIED=false
 
 
 # Resolve current public IP
 IP=$( curl --ipv4 -s http://icanhazip.com )
-# Update Google DNS Record
-URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${HOSTNAME}&myip=${IP}"
-OUTPUT=$(curl -s $URL)
 
-printf "Time:%s  " $DATE
-printf "%s  " $OUTPUT
-printf "\n"
+# Request current DNS record from Cloudflare
+DNS_RECORD=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+     -H "Authorization: Bearer $API_TOKEN" \
+     -H "Content-Type: application/json")
+
+# Check if IP has changed
+EXISTING_IP=$(echo $DNS_RECORD | jq -r '.result.content')
+if [ "$CURRENT_IP" == "$EXISTING_IP" ]; then
+    echo "IP has not changed. Exiting."
+    exit 0
+fi
+
+
+# Update the DNS record with the new IP
+UPDATE_RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+     -H "Authorization: Bearer $API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data "{\"type\":\"A\",\"name\":\"$DNS_NAME\",\"content\":\"$CURRENT_IP\",\"ttl\":$TTL,\"proxied\":$PROXIED}")
+
+# Check if the update was successful
+if echo $UPDATE_RESPONSE | jq -e '.success' > /dev/null; then
+    echo "DNS record updated successfully to $CURRENT_IP"
+    printf "Time:%s  " $DATE
+    printf "%s  " $OUTPUT
+    printf "\n"
+else
+    echo "Failed to update DNS record"
+    echo $UPDATE_RESPONSE | jq
+    exit 1
+fi
+
+
+
+
 
 
